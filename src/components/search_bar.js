f0165c92eed78464ed60087b94c9c3230767177b
Haichao Yu <haichaoy@asu.edu>
2017-06-16 22:39:11 -0700

    Initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+
+class SearchBar extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {term: ''};
+  }
+
+  render() {
+
+    return (
+      <div className="input-group search-bar">
+        <input
+          className="form-control"
+          placeholder="Search videos ..."
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
